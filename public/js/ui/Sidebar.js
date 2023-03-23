e989d1eb68e644000f5c4f53a9d6568c98d4fda3
Alexsey779 <fktrctq321@gmail.com>
2023-03-23 23:07:19 +0700

    push

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -18,7 +18,14 @@ class Sidebar {
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
+      const sidebarMini = document.querySelector('.sidebar-mini');
 
+    document.querySelector('.sidebar-toggle').addEventListener('click', (e) => {
+      e.preventDefault();
+
+      sidebarMini.classList.toggle('sidebar-open');
+      sidebarMini.classList.toggle('sidebar-collapse');
+    });
   }
 
   /**
@@ -30,5 +37,20 @@ class Sidebar {
    * */
   static initAuthLinks() {
 
+    document.querySelector('.menu-item_register a').addEventListener('click', () => {
+      App.getModal('register').open();
+    });
+
+    document.querySelector('.menu-item_login a').addEventListener('click', () => {
+      App.getModal('login').open();
+    });
+
+    document.querySelector('.menu-item_logout a').addEventListener('click', () => {
+      User.logout((err, response) => {
+        if (response && response.success) {
+          App.setState('init');
+        }
+      });
+    });
   }
 }
